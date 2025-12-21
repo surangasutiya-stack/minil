6471a0bb7779a207fc44cae130ba3fc26b894a67
surangasutiya-stack <surangasutiya@gmail.com>
2025-12-21 12:18:08 +0530

    Create index.js

---
+++ pair.js/index.js
@@ -0,0 +1,40 @@
+          
+ 
+//             N E D I Y A - M D
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Importing the 'pair' module
+import code from './pair.js';
+
+const app = express();
+
+// Resolve the current directory path in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 8000;
+
+import('events').then(events => {
+    events.EventEmitter.defaultMaxListeners = 500;
+});
+
+app.use('/code', code);
+app.use('/pair', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+app.use('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`𝐏𝙾𝚆𝙴𝚁𝙳 𝐁𝚈 NEDIYA MINI-𝐌𝙳\n\nServer running on http://localhost:${PORT}`);
+});
+
+export default app;
